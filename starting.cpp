f7f73ca4f5cbacf1ab7a66e90a6b796a191d908b
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-06-27 21:24:41 +0530

    recursion + memoization

--- starting.cpp
+++ starting.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 using namespace std;
 
@@ -16,4 +17,38 @@ int main(){
 
     return 0;
 }
+
+// Time Complexity: O(2^N)
+// Space Complexity: O(N) => {stack space}
+
+
 // In this solution, we are computing the previously computed value again like if we draw the recursion tree for fib(5) then we are repeating the calls. So, if we can somehow store the prev. computed values( called memoization ), we can save time.
+
+
+*/
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fibNumber(int n, vector<int> &dp) {
+    if(n <= 1) return n;
+
+    if(dp[n] != -1) return dp[n];   // here if n is prev. computed, then return it.
+
+    return dp[n] = fibNumber(n-1, dp) + fibNumber(n-2, dp);         // storing the nth value.
+}
+
+int main() {
+    int n; cin >> n;        // index.
+
+    vector<int> dp(n+1, -1);
+
+    cout << "fib: "<< fibNumber(n, dp);
+
+    return 0;
+}
+// Time Complexity: O(N)
+// Space Complexity: O(N)+O(N) => {stack space + extra array space.}
